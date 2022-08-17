a56b8a5bed77736773e5fc5da56071ef93941d8d
daniel-statsig <daniel@statsig.com>
2022-08-17 15:40:10 -0700

    bump version

--- Editor/SDKDetails.cs
+++ Editor/SDKDetails.cs
@@ -5,7 +5,7 @@ namespace StatsigUnity
     public static class SDKDetails
     {
         internal static string SDKType = "unity";
-        internal static string SDKVersion = "0.1.5";
+        internal static string SDKVersion = "0.1.6";
         internal static Dictionary<string, string> StatsigMetadata
         {
             get
